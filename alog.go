c99d0ed30cf531c0047cbf187060fabd02cd65ea
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2023-04-16 20:15:02 +0200

    Implement `Stop()` method

--- alog.go
+++ alog.go
@@ -99,6 +99,8 @@ func (al Alog) ErrorChannel() <-chan error {
 // Stop shuts down the logger. It will wait for all pending messages to be written and then return.
 // The logger will no longer function after this method has been called.
 func (al Alog) Stop() {
+	al.shutdownCh <- struct{}{}
+	_ = <-al.shutdownCompleteCh
 }
 
 // Write synchronously sends the message to the log output
