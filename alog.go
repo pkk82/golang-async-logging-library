9c4d56354b7409eacc262468a25d2347d6149dd6
pkk82@d2018-linux <piotrkrzysztofkozak@gmail.com>
2023-04-16 19:39:33 +0200

    Update `Start()` method

--- alog.go
+++ alog.go
@@ -38,7 +38,9 @@ func New(w io.Writer) *Alog {
 // Start begins the message loop for the asynchronous logger. It should be initiated as a goroutine to prevent
 // the caller from being blocked.
 func (al Alog) Start() {
-
+	for msg := range al.msgCh {
+		al.write(msg, nil)
+	}
 }
 
 func (al Alog) formatMessage(msg string) string {
